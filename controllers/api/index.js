550e0b5ab778700aec7d7e85d5a42019f76732d1
Bryce Spraggins <spragbry93@gmail.com>
2021-04-30 18:26:20 -0400

    sequelize and css

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,20 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+
 
 module.exports = router;
 
-/* not sure if I should use both or not
-
-const router = require('express').Router();
-
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;*/
